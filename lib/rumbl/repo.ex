39e90588594ff0b10990a2d5d023cbc46070180c
Cole R Lawrence <Colelawr@gmail.com>
2016-10-23 02:29:04 -0500

    fake users and routes

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,25 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  # use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository
+  """
+
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "Cole Lawrence", username: "cole", password: "elixir"},
+      %Rumbl.User{id: "2", name: "Luke Baker", username: "luke", password: "emily"},
+      %Rumbl.User{id: "3", name: "Thomas Vernon", username: "thomas", password: "docs"},
+      %Rumbl.User{id: "4", name: "Zach LaMarre", username: "zach", password: "sketch"}
+    ]
+  end
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, value} -> Map.get(map, key) == value end)
+    end
+  end
 end
