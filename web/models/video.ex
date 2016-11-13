31d43fd047b7dc1f330604eb87ab70ad7ba4a9e4
Cole R Lawrence <msgzht@gmail.com>
2016-11-13 17:14:18 -0600

    Add assoc_constraint to  video category

--- web/models/video.ex
+++ web/models/video.ex
@@ -11,12 +11,16 @@ defmodule Rumbl.Video do
     timestamps()
   end
 
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+
   @doc """
-  Builds a changeset based on the `struct` and `params`.
+  Builds a changeset based on the `model` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> assoc_constraint(:category)
+    # |> validate_required([:url, :title, :description])
   end
 end
