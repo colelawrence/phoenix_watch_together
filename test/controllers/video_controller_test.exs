fbcb15db15913b9012ceff0a749359195a9644ae
Cole R Lawrence <msgzht@gmail.com>
2016-11-17 11:51:23 -0600

    Add user videos filter test to video_controller_test

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,8 +1,30 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
-
-  alias Rumbl.Video
   
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(build_conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
+  # This gets passed to the above setup `config` argument
+  # Use keyword list 👇, or an atom
+  @tag login_as: "max"
+  # Specifying a single atom such as `@tag :logged_in`, is equivalent to `@tag logged_in: true`
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_video  = insert_video(user, title: "funny cats")
+    other_video = insert_video(insert_user(username: "other"), title: "another video")
+  
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
+  end
+
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
       get(conn, video_path(conn, :new)),
