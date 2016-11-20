7ad704a31a16862826223488eda9a855020816e2
Cole R Lawrence <msgzht@gmail.com>
2016-11-20 15:33:53 -0600

    Counter Explodes!

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -9,9 +9,19 @@ defmodule Rumbl.Counter do
     GenServer.start_link(__MODULE__, initial_val)
   end
 
-  def init(initial_val), do: {:ok, initial_val}
+  def init(initial_val) do
+    Process.send_after(self, :tick, 1000)
+    {:ok, initial_val}
+  end
 
   def handle_cast(:inc, val), do: {:noreply, val + 1}
   def handle_cast(:dec, val), do: {:noreply, val - 1}
   def handle_call(:val, _from, val), do: {:reply, val, val}
+
+  def handle_info(:tick, val) when val <= 0, do: raise "boom!"
+  def handle_info(:tick, val) do
+    IO.puts "tick #{val}"
+    Process.send_after(self, :tick, 1000)
+    {:noreply, val - 1}
+  end
 end
