df02f789fa797536681339260e6291a95e9e2b84
Cole R Lawrence <msgzht@gmail.com>
2016-11-20 12:50:30 -0600

    Remove unneeded category loading

--- web/controllers/category_controller.ex
+++ web/controllers/category_controller.ex
@@ -4,22 +4,6 @@ defmodule Rumbl.CategoryController do
   alias Rumbl.Video
 
   alias Rumbl.Category
-  plug :load_categories when action in [:show, :index]
-  defp load_categories(conn, _) do
-    query =
-      Category
-      |> Category.alphabetical
-      |> Category.names_and_ids
-    # here, I was very tempted to just chain everything together
-    # so, it would be categories = \ Category |> alpha |> names_and_ids |> Repo.all
-    # but, this seemed to be a little bit too much, in terms of understnading what is going on
-    # by separating the code we get a clear picture of how the algorithm is sectioned out.
-    categories = Repo.all query
-
-    conn
-    |> assign(:categories, categories)
-    # now available at conn.assigns.categories
-  end
 
   def index(conn, _params) do
     render(conn, "index.html")
