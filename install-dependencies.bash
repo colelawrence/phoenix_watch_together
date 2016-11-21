2f17b37dde1f755cee404603a860fff1a9098bd7
Cole R Lawrence <msgzht@gmail.com>
2016-11-21 17:16:17 -0600

    script for installing needed files to workspace through docker

---
+++ install-dependencies.bash
@@ -0,0 +1,9 @@
+docker-compose run web mix deps.get
+docker-compose run web mix compile
+docker-compose run web mix ecto.create
+
+docker-compose run web npm config set strict-ssl false
+docker-compose run web npm install
+
+# Seed database
+docker-compose run web mix run priv/repo/seeds.exs
