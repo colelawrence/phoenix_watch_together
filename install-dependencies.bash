3cb2572c093ab1fde2e8a6085c5bd4dd72156eb1
Cole R Lawrence <msgzht@gmail.com>
2016-11-21 17:24:48 -0600

    Needs to be migrated before seeding

--- install-dependencies.bash
+++ install-dependencies.bash
@@ -1,6 +1,7 @@
 docker-compose run web mix deps.get
 docker-compose run web mix compile
 docker-compose run web mix ecto.create
+docker-compose run web mix ecto.migrate
 
 docker-compose run web npm config set strict-ssl false
 docker-compose run web npm install
